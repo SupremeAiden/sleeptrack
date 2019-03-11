fa024df28d3299bca39eeec31dabb2eb5352cfb1
Troy <tengyl@uci.edu>
2019-03-10 17:14:21 -0700

    111

--- app/src/main/java/com/example/sleeptrack/MainActivity.java
+++ app/src/main/java/com/example/sleeptrack/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.sleeptrack;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void sendMessage(View view) {
-        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
