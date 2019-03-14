68c4e6072711c78c14fbc62dc891d2fd486418b2
Troy <tengyl@uci.edu>
2019-03-13 23:42:23 -0700

    working on spinner

--- app/src/main/java/com/example/sleeptrack/DailyLog.java
+++ app/src/main/java/com/example/sleeptrack/DailyLog.java
@@ -2,12 +2,28 @@ package com.example.sleeptrack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
-public class DailyLog extends AppCompatActivity {
+public class DailyLog extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_log);
+
+        Spinner mySpinner = ( Spinner ) findViewById(R.id.spinner);
+
     }
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos)
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
 }
