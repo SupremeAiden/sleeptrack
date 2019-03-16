d9df6329e7d4ae0fe6a4686cac0c87507fa25a73
Troy <tengyl@uci.edu>
2019-03-16 03:02:40 -0700

    working on spinner

--- app/src/main/java/com/example/sleeptrack/DailyLog.java
+++ app/src/main/java/com/example/sleeptrack/DailyLog.java
@@ -4,26 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.Spinner;
 
-public class DailyLog extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class DailyLog extends AppCompatActivity {
+
+    private Spinner spinner1, spinner2;
+    private Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_log);
 
-        Spinner mySpinner = ( Spinner ) findViewById(R.id.spinner);
+
 
     }
-    public void onItemSelected(AdapterView<?> parent, View view,
-                               int pos, long id) {
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-    }
+    public void addListenerOnButton() {
 
-    public void onNothingSelected(AdapterView<?> parent) {
-        // Another interface callback
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner2 = (Spinner) findViewById(R.id.spinner2);
+        spinner3 = (Spinner) findViewById(R.id.spinner3);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MyAndroidAppActivity.this,
+                        "OnClickListener : " +
+                                "\nSpinner 1 : "+ String.valueOf(spinner1.getSelectedItem()) +
+                                "\nSpinner 2 : "+ String.valueOf(spinner2.getSelectedItem()) +
+                                "\nSpinner 3 : "+ String.valueOf(spinner3.getSelectedItem()),
+                        Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 
 }
